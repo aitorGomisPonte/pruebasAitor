47261406b84f80e0988ee9538cf37e25f13b30f1
Aitor <aitor_enrique_gomis_tsapp1ma2021@cev.com>
2022-03-02 18:38:42 +0100

    Initial Commit

---
+++ pruebasAitor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pruebasAitor
+//
+//  Created by Aitor Gomis on 2/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
